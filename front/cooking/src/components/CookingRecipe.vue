b73766cd4d79ded658763026501fb2027392a77e
Carl Holms <holmscarl@gmail.com>
2022-04-21 11:47:14 +0200

    start scss

--- front/cooking/src/components/CookingRecipe.vue
+++ front/cooking/src/components/CookingRecipe.vue
@@ -1,8 +1,8 @@
 <template>
-    <article>
+    <article class="card">
         <img :src="getImageUrl">
         <h2>{{recipe.title.rendered}}</h2>
-        <div v-html="recipe.excerpt.rendered">
+        <div class="card__content" v-html="recipe.excerpt.rendered">
         </div>
     </article>
 </template>
@@ -19,13 +19,14 @@ export default {
             if (this.recipe._embedded['wp:featuredmedia'] && this.recipe._embedded['wp:featuredmedia'][0].media_details && this.recipe._embedded['wp:featuredmedia'][0].media_details.sizes){
                 return this.recipe._embedded['wp:featuredmedia'][0].source_url;
             } else {
-                return 'https://picsum.photos/seed/picsum/200/300'
+                return 'https://picsum.photos/seed/picsum/400/300'
             }
         }
     }
 }
 </script>
 
-<style>
+<style scoped lang=scss>
+
 
 </style>
--- front/cooking/src/components/CookingRecipeList.vue
+++ front/cooking/src/components/CookingRecipeList.vue
@@ -37,6 +37,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang=scss>
+
 
 </style>
